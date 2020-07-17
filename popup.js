6b3c536c58ead8ba3f7b9c8c402cb2026a20cdf7
kender-mendoza <kendermendoza591@outlook.com>
2020-07-17 11:13:59 -0600

    hotfix-btn_delete-001

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ const addElement = (url) => {
             class="svg-inline--fa fa-times fa-w-11 icon-delete" role="img" xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 352 512">
             <path fill="currentColor"
-                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z">
+                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class="btn-delete">
             </path>
         </svg>
     </div>
@@ -30,10 +30,14 @@ getElementSaved()
 
 // ? delete elemento
 const deleteElement = () => {
-    const elements = document.getElementsByClassName("icon-delete")
-    for (const iterator of elements) {
-        iterator.addEventListener('click',(e)=>{
-            e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode)
+    const elements = document.getElementsByClassName("btn-delete")
+    for (const element of elements) {
+        element.addEventListener('click',(e)=>{
+            let markerContainer = e.target.parentNode.parentNode.parentNode // ? seleccionando el elemento padre de los markers
+            let marker = e.target.parentNode.parentNode // ? selecciona el marker
+
+            markerContainer.removeChild(marker) // ? eliminando dela vista
+            // ? eliminando desde el storange
         })
     }
 }
