5e50037d8a4b9f441c27654010fdc35e58320dca
kender-mendoza <kendermendoza591@outlook.com>
2020-07-21 21:30:05 -0600

    Optimizando el codigo js

--- popup.js
+++ popup.js
@@ -1,67 +1,67 @@
-// ? funcion encargada crear el elemento html
-const addElement = (url) => {
-    document.getElementById('container').innerHTML += `
-    <div class="marker">
-        <a href="http://www.${url.trim()}" target="_blank" class="info-container">
+const markerContainer = document.getElementById('container')
+
+const createElementHtml = (url) => {
+    const element = `
+    <div class="marker" id="${url}">
+        <a href="http://www.${url}" target="_blank" class="info-container">
             <img src="https://www.freeiconspng.com/uploads/pikachu-png-icon-6.png   " alt="page logo"
                 class="page-logo">
-            <p class="page-name">${url.trim()}</p>
+            <p class="page-name">${url}</p>
         </a>
         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times"
             class="svg-inline--fa fa-times fa-w-11 icon-delete" role="img" xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 352 512">
             <path fill="currentColor"
-                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class="btn-delete" id="${url.trim()}">
+                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" class="btn-delete">
             </path>
         </svg>
     </div>
     `
+    return element
 }
 
-// ? Local storange
-const getElementSaved = () => {
-    for (const key in localStorage) {
-        if (localStorage.hasOwnProperty(key)) {
-            addElement(localStorage[key])
-        }
-    }
-}
-getElementSaved()
-
-// ? eliminar elemento del storange
-const deleteElementSaved = (key) =>{
-    localStorage.removeItem(key)
-}
-
-// ? delete elemento
-const deleteElement = () => {
-    const elements = document.getElementsByClassName("btn-delete")
-    for (const element of elements) {
-        element.addEventListener('click',(e)=>{
-            let markerContainer = e.target.parentNode.parentNode.parentNode // ? seleccionando el elemento padre de los markers
-            let marker = e.target.parentNode.parentNode // ? selecciona el marker
-
-            markerContainer.removeChild(marker) // ? eliminando dela vista
-            deleteElementSaved(e.target.id) // ? eliminando desde el storange
+const deleteElementHtml = () => {
+    const btnsDelete = document.getElementsByClassName("btn-delete")
+    for (const btn of btnsDelete) {
+        btn.addEventListener('click', (e) => {
+            const marker = e.target.parentNode.parentNode
+            markerContainer.removeChild(marker)
+            localStorage.removeItem(marker.id)
         })
     }
 }
-deleteElement()
 
-// ? esta funcion agrega los alementos (markers) 
-const addEvent = () => {
-    const input = document.getElementById('input')
+/* Esta funcion es para poder reasignar los eventos a los elementos
+correspondientes, ya que al agregar elementos nuevos a la vista en
+este caso el contenedor, estos se renderizan por cada elemento que
+se agrega, por esta razon se les debe de reasignar los eventos */
+const addElementHtml = () => {
     document.getElementById('btn-add').addEventListener('click', () => {
-        if (input.value) {
-            let url = input.value
-            // guardando elemento en el local storange
-            addElement(url)
+        let url = document.getElementById('input').value.trim()
+
+        if (url) {
+            // ? agregando elemento (marker)
+            markerContainer.innerHTML += createElementHtml(url)
+            // ? agregando elemento al localStorange
             localStorage.setItem(url, url)
-            addEvent()
-            deleteElement()
-            /* utiliza recursion para reasignar los eventos al head, aun no se si
-                esta solucion es la correcta.*/
+            // ? limpiando el input
+            url.value = ''
+            // ? reasignando los eventos
+            addElementHtml()
+            deleteElementHtml()
         }
     })
 }
-addEvent()
+
+const showElementsSaved = () => {
+    for (const key in localStorage) {
+        if (localStorage.hasOwnProperty(key)) {
+            markerContainer.innerHTML += createElementHtml(localStorage[key])
+        }
+    }
+
+    addElementHtml()
+    deleteElementHtml()
+}
+
+showElementsSaved()
